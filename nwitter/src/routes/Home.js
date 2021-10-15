05f8a2bb47fcdeb95c8e773da0fd62eb20f7a53d
HWANG JANG WON <star007lg@gmail.com>
2021-10-15 21:03:49 +0900

    Show Tweet(#3.2)

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
-import { addDoc, collection } from "@firebase/firestore";
+import { addDoc, collection, getDocs, query } from "@firebase/firestore";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const q = query(collection(dbService, "nweets"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+        const nweetObj = {
+        ...doc.data(),
+        id: doc.id,
+        }
+        setNweets(prev => [nweetObj, ...prev]);
+        });
+        };
+        useEffect(() => {
+        getNweets();
+        }, []);
     const onSubmit = async(event) => {
         event.preventDefault();
         try {
@@ -30,6 +45,13 @@ const Home = () => {
                 <input value = {nweet} onChange={onChange} type = "text" placeholder="What's on your mind?" maxLength={120} />
                 <input type= "submit" value = "Nweet" />
             </form>
+            <div>
+                {nweets.map(nweet => (
+                <div key = {nweet.id}>
+                    <h4>{nweet.nweet}</h4>
+                </div>
+                ))}
+            </div>
         </div>
     );
 };
